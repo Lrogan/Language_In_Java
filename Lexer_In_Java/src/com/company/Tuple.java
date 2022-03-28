ea2f9b8205197f6aec53d91b0d1f53bdd9e145db
LROGANMAG15\antho <thesecretsmurfservice@gmail.com>
2022-03-27 20:56:12 -0500

    ignoring some nonsense

--- Lexer_In_Java/src/com/company/Tuple.java
+++ Lexer_In_Java/src/com/company/Tuple.java
@@ -1,2 +1,34 @@
-package com.company;public class Tuple {
+package com.company;
+
+
+
+public class Tuple
+{
+    Tuple t = null;
+    Lexer.Lexeme l;
+    String p = "";
+
+    public Tuple(Lexer.Lexeme lex, String prog)
+    {
+        l = lex;
+        p = prog;
+    }
+
+    public Tuple(Tuple token, String prog)
+    {
+        t = token;
+        p = prog;
+    }
+
+    public Lexer.Lexeme getLexeme() {
+        return l;
+    }
+
+    public String getProgString() {
+        return p;
+    }
+
+    public Tuple getT() {
+        return t;
+    }
 }
