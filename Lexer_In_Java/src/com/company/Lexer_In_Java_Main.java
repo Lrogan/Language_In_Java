6330a30617ddf4ea02d7c01e508bf77212b9b529
LROGANMAG15\antho <thesecretsmurfservice@gmail.com>
2022-03-27 21:01:53 -0500

    Removed the testing line

--- Lexer_In_Java/src/com/company/Lexer_In_Java_Main.java
+++ Lexer_In_Java/src/com/company/Lexer_In_Java_Main.java
@@ -7,15 +7,14 @@ public class Lexer_In_Java_Main {
     public static void main(String[] args) throws Exception {
         String prog = "";
         Lexer lexical = new Lexer();
-        prog = lexical.getInput("tester.txt");
-//        if(args.length > 0)
-//            prog = lexical.getInput(args[0]);
-//        else
-//        {
-//            Scanner keyboard = new Scanner(System.in);
-//            System.out.println("Please input the full name(including the extension) of the program you wish to lexicalize\neg. \"testProgram.txt\"");
-//            prog = lexical.getInput(keyboard.nextLine());
-//        }
+        if(args.length > 0)
+            prog = lexical.getInput(args[0]);
+        else
+        {
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Please input the full name(including the extension) of the program you wish to lexicalize\neg. \"testProgram.txt\"");
+            prog = lexical.getInput(keyboard.nextLine());
+        }
 
         Tuple lexd = lexical.lex(prog);
         while(prog.length() > 0)
